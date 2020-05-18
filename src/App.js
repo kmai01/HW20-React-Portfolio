c4a08ad156cac687668e3edfca702d9312fd4e1c
Khanh Mai <57655821+kmai01@users.noreply.github.com>
2020-05-18 01:32:46 -0500

    change to app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <Router>
+      <div>
+        <NavTabs/>
+
+      </div>
+
+    </Router>
+  
+  
+      
        
-       
-      </header>
-    </div>
+    
   );
 }
 
