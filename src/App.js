ff54edc5f100a45e1400a7cbcafc93f0b6968f38
Khanh Mai <57655821+kmai01@users.noreply.github.com>
2020-05-19 22:55:11 -0500

    add nav button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
+import NavTabs from "./components/NavTabs";
+import About from "./components/pages/About";
+import Portfolio from "./components/pages/Portfolio";
+import Contact from "./components/pages/Contact";
+
 
 function App() {
   return (
     <Router>
       <div>
         <NavTabs/>
-       
-        <Route exact path="/about" component={About} />
-        <Route exact path="/portfolio" component={About} />
+      
+        <Route exact path="/" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
         <Route path="/contact" component={Contact} />
 
       </div>
