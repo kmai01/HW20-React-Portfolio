0eb682ca00938572a5ec1f19e082941ecfa4d405
unknown <kmaibox@hotmail.com>
2020-05-22 00:49:19 -0500

    portfolio

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -1,4 +1,14 @@
 import React from "react";
+import lovequest from "../Assets/images/lovequest.PNG";
+import get_media from "../Assets/images/get_media.PNG";
+import notetaker from "../Assets/images/notetaker.PNG";
+import weather from "../Assets/images/weather.PNG";
+import work_day from "../Assets/images/Work_day.PNG";
+import visual from "../Assets/images/Visual_Studio_Code_logo.png";
+import lifecycle from "../Assets/images/software_development_lifecycle.png";
+import pcbuilding from "../Assets/images/PCbuilding.png";
+
+
 
 function Portfolio() {
   return (
@@ -12,7 +22,7 @@ function Portfolio() {
           <div className="card text-center">
 
             <a href="https://morning-fjord-37921.herokuapp.com/">
-              <img className="card-img" src="Assets/images/lovequest.PNG" alt="blank"/></a>
+              <img className="card-img" src={lovequest} alt="blank"/></a>
 
           </div>
         </div>
@@ -23,7 +33,7 @@ function Portfolio() {
             <div className="card text-center">
 
               <a href="https://ctunnell.github.io/project-i/">
-                <img className="card-img" src="Assets/images/get_media.PNG" alt="blank"/></a>
+                <img className="card-img" src={get_media} alt="blank"/></a>
 
                 </div>
             </div>
@@ -33,7 +43,7 @@ function Portfolio() {
               <div className="card text-center">
 
                 <a href="https://stark-caverns-10185.herokuapp.com/">
-                  <img className="card-img" src="Assets/images/notetaker.PNG" alt="blank"/></a>
+                  <img className="card-img" src={notetaker} alt="blank"/></a>
 
 
               </div>
@@ -44,7 +54,7 @@ function Portfolio() {
                 <div className="card text-center">
 
                   <a href="https://kmai01.github.io/Homework-6">
-                    <img className="card-img" src="Assets/images/weather.PNG" alt="blank"/></a>
+                    <img className="card-img" src={weather} alt="blank"/></a>
 
 
                 </div>
@@ -59,7 +69,7 @@ function Portfolio() {
                   <div className="card text-center">
 
                     <a href="https://kmai01.github.io/Homework-5">
-                      <img className="card-img" src="Assets/images/Work_day.PNG" alt="blank"/></a>
+                      <img className="card-img" src={work_day} alt="blank"/></a>
 
 
                 </div>
@@ -69,10 +79,10 @@ function Portfolio() {
 
                     <div className="card text-center">
 
-                      <img className="card-img" src="Assets/images/Visual_Studio_Code_logo.png" alt="blank"/>
+                      <img className="card-img" src={visual} alt="blank"/>
 
                         <div className="card-img-overlay">
-                          <h3 class="card-title">VS Code</h3>
+                          <h3 className="card-title">VS Code</h3>
                         </div>
 
                 </div>
@@ -83,7 +93,7 @@ function Portfolio() {
                       <div className="card text-center">
 
 
-                        <img className="card-img" src="Assets/images/software_development_lifecycle.png" alt="blank"/>
+                        <img className="card-img" src={lifecycle} alt="blank"/>
 
                           <div className="card-img-overlay">
                             <h3 className="card-title">SDLC</h3>
@@ -94,7 +104,7 @@ function Portfolio() {
                       <div className="col-sm-3">
 
                         <div className="card text-center">
-                          <img className="card-img" src="Assets/images/PCbuilding.png" alt="blank"/>
+                          <img className="card-img" src={pcbuilding} alt="blank"/>
 
                             <div className="card-img-overlay">
                               <h3 className="card-title">PC Building</h3>
